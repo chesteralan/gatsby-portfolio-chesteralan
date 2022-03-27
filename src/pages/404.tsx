13325d661b647dd2dc9c514d015cd24525520802
Chester Alan <chesteralantagudin@gmail.com>
2022-03-28 01:45:58 +0800

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,70 @@
 import React from 'react'
+import PageCover from '../components/PageCover/PageCover'
+import Header from '../components/Header/Header'
+import AboutSection from '../components/AboutSection/AboutSection'
+import ContactSection from '../components/ContactSection/ContactSection'
+import Footer from '../components/Footer/Footer'
+import BlogSection from '../components/BlogSection/BlogSection'
+import TestimonialsSection from '../components/TestimonialsSection/TestimonialsSection'
+import PortfolioSection from '../components/PortfolioSection/PortfolioSection'
+import ServicesSection from '../components/ServicesSection/ServicesSection'
+import ExperienceSection from '../components/ExperienceSection/ExperienceSection'
+import SkillsSection from '../components/SkillsSection/SkillsSection'
 
-const NotFoundPage = () => {
-  return (
-    <div>NotFoundPage</div>
+import {Helmet} from "react-helmet";
+import { useStaticQuery, graphql } from 'gatsby'
+
+const IndexPage = () => {
+
+	const data = useStaticQuery(graphql`
+	query {
+		contentYaml(
+			fields: {fileName: {eq: "page-sections-cover"}, parentFolder: {eq: "content"}}
+		) {
+		  title
+		  display_name
+		}
+	  }
+	`);
+
+	const { display_name, title } = data.contentYaml;
+
+  return (<>
+  <Helmet title={`${display_name} - ${title}`}>
+  </Helmet>
+    <div className="site-container">
+			
+			<Header />
+
+			<div className="page-wrapper">
+				<PageCover />
+
+				<div className="page-content">
+
+					<AboutSection />
+
+					<SkillsSection />
+
+					<ExperienceSection />
+
+					<ServicesSection />
+
+					<PortfolioSection />
+
+					<TestimonialsSection />
+
+					<BlogSection />
+
+					<ContactSection />
+
+					<Footer />
+
+				</div>
+			</div>
+
+		</div>
+		</>
   )
 }
 
-export default NotFoundPage
+export default IndexPage
